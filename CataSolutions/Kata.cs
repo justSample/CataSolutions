aa301f8919175040bf820f1a072c227a40cb26e6
Kostia Kravtsov <just.sample@mail.ru>
2021-11-09 09:50:37 +0700

    Kata Tower.

---
+++ CataSolutions/Kata.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CataSolutions
+{
+    public class Kata
+    {
+
+        public static string[] TowerBuilder(int nFloors)
+        {
+            //if (nFloors == 1) return new string[] { "*" };
+
+            char towerChar = '*';
+
+            string[] tower = new string[nFloors];
+
+            int totalCount = 1;
+
+            for (int i = 1; i < nFloors; i++)
+            {
+                totalCount += 2;
+            }
+
+
+            int increesingNumber = 1;
+
+            for (int i = 0; i < tower.Length; i++)
+            {
+                char[] rowChars = new char[totalCount];
+
+                for (int j = 0; j < totalCount; j++)
+                {
+                    rowChars[j] = ' ';
+                }
+
+                for (int j = 0; j < increesingNumber; j++)
+                {
+                    int indexForSetChar = (totalCount / 2) + i;
+
+                    indexForSetChar -= j;
+
+                    rowChars[indexForSetChar] = towerChar;
+
+                }
+
+                tower[i] = new string(rowChars);
+
+                increesingNumber += 2;
+            }
+
+            return tower;
+        }
+
+    }
+}
---
+++ CataSolutionsTests/KataTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CataSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CataSolutions.Tests
+{
+    [TestClass]
+    public class KataTests
+    {
+
+
+
+
+
+        [TestMethod]
+        public void SampleTest()
+        {
+            var check = Kata.TowerBuilder(7);
+        }
+
+        
+    }
+}
